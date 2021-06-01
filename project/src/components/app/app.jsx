924774c4ad352b3bdc26e6498343de2e44849b25
Pavelliss <kovrizhkoPavel@gmail.com>
2021-06-01 22:09:38 +0300

    module1-task3

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {cardCount} = props;
+
+  return (
+    <MainPage cardCount={cardCount}/>
+  );
 }
+
+App.propTypes = {
+  cardCount: PropTypes.number.isRequired,
+};
 
 export default App;
